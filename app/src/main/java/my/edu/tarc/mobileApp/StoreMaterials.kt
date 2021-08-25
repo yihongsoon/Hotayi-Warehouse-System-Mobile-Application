cc3cc13f1696f7c335defe8acd83297aad44a4f0
DouglasLee1204 <douglaslyz-pm18@student.tarc.edu.my>
2021-08-26 00:26:24 +0800

    Store

---
+++ app/src/main/java/my/edu/tarc/mobileApp/StoreMaterials.kt
@@ -0,0 +1,11 @@
+package my.edu.tarc.mobileApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreMaterials : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store_materials)
+    }
+}
