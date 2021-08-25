5216685926df6df7f89f821bcbf29cf8c74bcd8d
yihongsoon <soonyh-pm18@student.tarc.edu.my>
2021-08-26 00:49:03 +0800

    Report Soon

---
+++ app/src/main/java/my/edu/tarc/mobileApp/Report.kt
@@ -0,0 +1,11 @@
+package my.edu.tarc.mobileApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Report : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
