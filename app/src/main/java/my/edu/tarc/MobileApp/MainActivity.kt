0b49871413539468be456d0daa3f5c3893c87171
yihongsoon <soonyh-pm18@student.tarc.edu.my>
2021-08-25 22:07:50 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/MobileApp/MainActivity.kt
@@ -0,0 +1,11 @@
+package my.edu.tarc.MobileApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
