72d15d678168fcfb5d1448bde06dab61db66d210
jjchong <jiajunchong2000@hotmail.com>
2021-08-25 23:37:30 +0800

    test

---
+++ app/src/main/java/my/edu/tarc/MobileApp/RetrieveMaterials.kt
@@ -0,0 +1,11 @@
+package my.edu.tarc.MobileApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RetrieveMaterials : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retrieve_materials)
+    }
+}
