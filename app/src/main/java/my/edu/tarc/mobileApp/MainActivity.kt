96ca9821276d0137070c8d2f2e30f07752918ca1
DouglasLee1204 <douglaslyz-pm18@student.tarc.edu.my>
2021-09-07 01:33:37 +0800

    main

--- app/src/main/java/my/edu/tarc/mobileApp/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/mobileApp/MainActivity.kt
@@ -1,11 +1,34 @@
 package my.edu.tarc.mobileApp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnStore = findViewById<Button>(R.id.btnStore)
+        val btnReceive = findViewById<Button>(R.id.btnReceive)
+        val btnRetrieve = findViewById<Button>(R.id.btnRetrieve)
+        val btnReport = findViewById<Button>(R.id.btnReport)
+
+        btnStore.setOnClickListener{
+            val intent = Intent(this, StoreMaterials::class.java)
+            startActivity(intent)
+        }
+        btnReceive.setOnClickListener{
+            val intent = Intent(this, Receive::class.java)
+            startActivity(intent)
+        }
+        btnRetrieve.setOnClickListener{
+            val intent = Intent(this, RetrieveMaterials::class.java)
+            startActivity(intent)
+        }
+        btnReport.setOnClickListener{
+            val intent = Intent(this, Report::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/my/edu/tarc/mobileApp/StoreMaterials.kt
+++ app/src/main/java/my/edu/tarc/mobileApp/StoreMaterials.kt
@@ -1,11 +1,35 @@
 package my.edu.tarc.mobileApp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+
+import androidx.appcompat.app.AppCompatActivity
+
 
 class StoreMaterials : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_store_materials)
+
+        val btnScan = findViewById<Button>(R.id.btnScanMaterial)
+
+        btnScan.setOnClickListener{
+            val intent = Intent(this, Scanner::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
+
+
+
+
+
+
+
+
 }
