962f2627aa19722c440b8f1e61f19a4c10560110
kelvinldq-pm18@student.tarc.edu.my <kelvinldq-pm18@student.tarc.edu.my>
2021-09-11 01:02:43 +0800

    receiveMaterial store to database

---
+++ app/src/main/java/my/edu/tarc/mobileApp/Material.kt
@@ -0,0 +1,7 @@
+package my.edu.tarc.mobileApp
+
+import java.util.*
+
+class Material(val serial:String? = null, val part:String? = null, val qty:String? = null, val rackid:String?=null, val rackin:String?=null,
+               val rackno:String?=null, val rackout:String?=null, val receivedate:String?=null, val staffid:String?=null, val status:String?=null) {
+}
--- app/src/main/java/my/edu/tarc/mobileApp/Receive.kt
+++ app/src/main/java/my/edu/tarc/mobileApp/Receive.kt
@@ -1,6 +1,7 @@
 package my.edu.tarc.mobileApp
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import org.w3c.dom.Text
 import java.text.SimpleDateFormat
 import java.util.*
@@ -16,6 +19,8 @@ import java.util.*
 class Receive : AppCompatActivity(){
 
     private lateinit var firebaseAuth : FirebaseAuth
+
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -39,12 +44,10 @@ class Receive : AppCompatActivity(){
         val scanningStatus = intent.getStringExtra("scanStatus")
 
         val emailStaff = firebaseAuth.currentUser?.email.toString()
-        //val database = FirebaseDatabase.getInstance()
-        //val myReference = database.getReference("Material")
 
-        /*val Material = Material(serial=serialNo.text.toString(),part=partNo.text.toString(),qty=qty.text.toString(),
-                    receivedate=txtDate.text.toString(),status=status.text.toString(),staffid=staffID.text.toString(),rackid="",rackin="",rackno="",rackout="")
-                myReference.child(Material.serialNo.text.toString()).child("serial").setValue(Material.serial)*/
+        val database = FirebaseDatabase.getInstance()
+        val myReference = database.getReference("Material")
+
 
         if(scanningStatus == "true"){
             val splitPart = partCode?.split("-")
@@ -56,11 +59,14 @@ class Receive : AppCompatActivity(){
                 val showPartNo = splitPart?.get(0)
                 val showQty = splitPart?.get(1)
 
-                val randomNumber = (100000000..999999999).random()
+                val randomNumber = (10000000..99999999).random()
 
                 val txtDate = findViewById<TextView>(R.id.txtRecDate)
                 val df = SimpleDateFormat("dd/MM/yyyy")
                 val currentDate = df.format(Calendar.getInstance().time)
+
+                val dateFormat = SimpleDateFormat("ddMMyyyy")
+                val serialDate = dateFormat.format(Calendar.getInstance().time)
 
                 val partNo = findViewById<TextView>(R.id.txtPartNo)
                 val qty = findViewById<TextView>(R.id.txtQuantity)
@@ -72,9 +78,26 @@ class Receive : AppCompatActivity(){
                 partNo.text = showPartNo
                 qty.text = showQty
                 status.text = "RECEIVED"
-                serialNo.text = "HWS"+ randomNumber.toString()
+                serialNo.text = "S"+ serialDate + randomNumber.toString() + "HW"
                 staffID.text = emailStaff
 
+                val rackid = ""
+                val rackno = ""
+                val rackin = ""
+                val rackout = ""
+
+                val Material = Material(serial=serialNo.text.toString(),part=partNo.text.toString(), qty = qty.text.toString(),
+                    receivedate=txtDate.text.toString(),status=status.text.toString(),staffid=staffID.text.toString(),rackid=rackid.toString(),rackno=rackno.toString(),rackin=rackin.toString(),rackout=rackout.toString())
+                myReference.child(Material.serial.toString()).child("serial").setValue(Material.serial)
+                myReference.child(Material.serial.toString()).child("part").setValue(Material.part)
+                myReference.child(Material.serial.toString()).child("qty").setValue(Material.qty)
+                myReference.child(Material.serial.toString()).child("rackid").setValue(Material.rackid)
+                myReference.child(Material.serial.toString()).child("rackno").setValue(Material.rackno)
+                myReference.child(Material.serial.toString()).child("rackin").setValue(Material.rackin)
+                myReference.child(Material.serial.toString()).child("rackout").setValue(Material.rackout)
+                myReference.child(Material.serial.toString()).child("receivedate").setValue(Material.receivedate)
+                myReference.child(Material.serial.toString()).child("status").setValue(Material.status)
+                myReference.child(Material.serial.toString()).child("receiveby").setValue(Material.staffid)
             }else{
                 Toast.makeText(applicationContext, "Wrong Barcode Scanned", Toast.LENGTH_SHORT).show()
             }
