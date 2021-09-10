de0269045cadcd12132acd8e3137f29fccd5b01b
DouglasLee1204 <douglaslyz-pm18@student.tarc.edu.my>
2021-09-10 21:10:53 +0800

    store

---
+++ app/src/main/java/my/edu/tarc/mobileApp/RackScanner.kt
@@ -0,0 +1,114 @@
+package my.edu.tarc.mobileApp
+
+import android.app.ProgressDialog.show
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.*
+
+private const val CAMERA_REQUEST_CODE = 101
+
+class RackScanner : AppCompatActivity() {
+    private lateinit var codeScanner: CodeScanner
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rack_scanner)
+
+        val intent = intent
+
+        val scanningStatus = intent.getStringExtra("scanStatus")
+
+        if(scanningStatus=="true"){
+            setupPermission()
+            codeScanner()
+        }
+
+    }
+
+    private fun codeScanner(){
+        val intent = intent
+        val scanner = findViewById<CodeScannerView>(R.id.ScannerView)
+        val text = findViewById<TextView>(R.id.textViewScanner)
+        val qrTest = intent.getStringExtra("storeCodeContent")
+        codeScanner = CodeScanner(this, scanner)
+        codeScanner.apply{
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread{
+                    text.text = it.text
+                    var strSerial = qrTest
+                    var strRack= text.text
+                    Log.d("SerialCodeContent", strSerial.toString())
+                    Log.d("RackCodeContent", strRack.toString())
+
+                    val intent = Intent(this@RackScanner, StoreMaterials::class.java)
+                    intent.putExtra("serialCodeContent", strSerial)
+                    intent.putExtra("rackCodeContent", strRack)
+                    intent.putExtra("scanStatus","true")
+                    startActivity(intent)
+                }
+            }
+            errorCallback = ErrorCallback {
+                runOnUiThread{
+                    Log.e("Main", "Camera initialization error: ${it.message}")
+                }
+            }
+        }
+        scanner.setOnClickListener{
+            codeScanner.startPreview()
+        }
+    }
+    override fun onResume(){
+        super.onResume()
+        codeScanner.startPreview()
+    }
+    override fun onPause(){
+        codeScanner.releaseResources()
+        super.onPause()
+
+    }
+
+    private fun setupPermission(){
+        val permission = ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.CAMERA)
+
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            CAMERA_REQUEST_CODE -> {
+                if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"You need the camera permission to be able to scan",Toast.LENGTH_SHORT)
+
+                }
+                else{
+
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/my/edu/tarc/mobileApp/StoreMaterials.kt
+++ app/src/main/java/my/edu/tarc/mobileApp/StoreMaterials.kt
@@ -3,9 +3,14 @@ package my.edu.tarc.mobileApp
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class StoreMaterials : AppCompatActivity() {
@@ -15,10 +20,62 @@ class StoreMaterials : AppCompatActivity() {
         setContentView(R.layout.activity_store_materials)
 
         val btnScan = findViewById<Button>(R.id.btnScanMaterial)
+        val btnSave = findViewById<Button>(R.id.btnStoreSave)
 
         btnScan.setOnClickListener{
-            val intent = Intent(this, Scanner::class.java)
+            val intent = Intent(this, StoreScanner::class.java)
             startActivity(intent)
+        }
+
+        val intent = intent
+        val serialCode = intent.getStringExtra("serialCodeContent")
+        val rackCode = intent.getStringExtra("rackCodeContent")
+        val scanningStatus = intent.getStringExtra("scanStatus")
+
+        if(scanningStatus == "true"){
+            val splitSerial = serialCode?.split(",")
+            val splitRack = rackCode?.split(",")
+
+            val resultSerial = splitSerial?.count()
+            val resultRack = splitRack?.count()
+            Log.d("CountSerial", resultSerial.toString())
+            Log.d("CountRack", resultRack.toString())
+
+            if(resultSerial == 2 && resultRack ==2){
+
+                val displayStoreRackID = splitRack?.get(0)
+                val displayStoreRackNo = splitRack?.get(1)
+
+                val displayStorePartNo = splitSerial?.get(0)
+                val displayStoreSerialNo = splitSerial?.get(1)
+
+                val storeRackID = findViewById<TextView>(R.id.txtStoreRackID)
+                val storeRackNo = findViewById<TextView>(R.id.txtStoreRackNo)
+                val storePartNo = findViewById<TextView>(R.id.txtPartNo)
+                val storeSerialNo = findViewById<TextView>(R.id.txtStoreSerialNumber)
+                val storeStatus = findViewById<TextView>(R.id.txtStoreStatus)
+                val storeRackInDate = findViewById<TextView>(R.id.txtRackInDate)
+                val storeQty = findViewById<TextView>(R.id.txtStoreQty)
+
+                storeRackID.text= displayStoreRackID
+                storeRackNo.text= displayStoreRackNo
+
+                storePartNo.text = displayStorePartNo
+                storeSerialNo.text = displayStoreSerialNo
+
+                storeStatus.text = "STORED"
+
+                val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+                val currentDate = sdf.format(Date())
+
+                storeRackInDate.text = currentDate
+                storeQty.text = "1"
+            }
+            else{
+                Toast.makeText(applicationContext, "Wrong Barcode Scanned", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
         }
 
     }
